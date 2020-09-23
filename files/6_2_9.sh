f4d78baccbb9b59872a9e5a8c094a997d4dbb245
Ali Baker <alivxlive@gmail.com>
2020-09-24 00:49:05 +0300

    Fix lint2

--- files/6_2_9.sh
+++ files/6_2_9.sh
@@ -3,10 +3,8 @@ grep -E -v '^(root|halt|sync|shutdown)' /etc/passwd | awk -F: '($7 !="'"$(which 
     if [ ! -d "$dir" ]; then
         echo "The home directory ($dir) of user $user does not exist."
     else
-        for file in $dir/.rhosts; do
-            if [ ! -h "$file" -a -f "$file" ]; then
-                echo ".rhosts file in $dir"
-            fi
-        done
+        if [ ! -h "$dir/.netrc" -a -f "$dir/.netrc" ]; then
+            echo ".netrc file $dir/.netrc exists"
+        fi
     fi
 done
